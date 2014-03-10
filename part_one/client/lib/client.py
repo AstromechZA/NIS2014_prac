0b1a9040357c912b69e27c532042ea26fa58053a
Ben Meier <benmeier42@gmail.com>
2014-03-10 19:56:59 +0200

    added verification / signing example

--- part_one/client/lib/client.py
+++ part_one/client/lib/client.py
@@ -1,6 +1,10 @@
 import socket
 import json
 import os
+import base64
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
+from Crypto.Signature import PKCS1_v1_5
 
 def load_config():
     """ Load config object from client configuration file """
@@ -8,7 +12,23 @@ def load_config():
     with open(path) as f:
         return json.loads(f.read())
 
+def load_key_from_file(path):
+    """ Create key from file """
+    p = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', path)
+    with open(os.path.abspath(p)) as f:
+        return RSA.importKey(f.read())
+
 cfg = load_config()
+
+server_key = load_key_from_file('keyring/server.pub')
+my_key = load_key_from_file('keyring/self.pem')
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((cfg['server'], cfg['port']))
-s.send('hello')
+
+client_id = cfg['id']
+hash_of_id = SHA.new(client_id)
+signer = PKCS1_v1_5.new(my_key)
+signed = base64.b64encode(signer.sign(hash_of_id))
+
+s.send("%s|%s" % (client_id, signed))
--- part_one/server/lib/connection_handler.py
+++ part_one/server/lib/connection_handler.py
@@ -1,4 +1,11 @@
 import logging
+import base64
+
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
+from Crypto.Signature import PKCS1_v1_5
+
+from key_things import load_key_from_file
 
 logger = logging.getLogger('nisprac.connhandler')
 
@@ -9,8 +16,14 @@ class ConnectionHandler(object):
         super(ConnectionHandler, self).__init__()
         self.conn = conn
         self.cfg = cfg
+        self.my_key = load_key_from_file('keyring/self.pem')
 
     def process(self):
         logger.info('Processing')
-        data = self.conn.recv(1024)
-        print data
+        data = self.conn.recv(4096)
+        parts = data.split('|')
+        cid = parts[0]
+        c_key = load_key_from_file("keyring/%s.pub" % cid)
+        c_signer = PKCS1_v1_5.new(c_key)
+        r = base64.b64decode(parts[1])
+        print c_signer.verify(SHA.new(cid), r)
