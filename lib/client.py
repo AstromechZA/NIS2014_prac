57c18638b435b22a521b9a2333e70c15e23dcd08
Ben Meier <benmeier42@gmail.com>
2014-03-10 16:23:49 +0200

    added client config reading

--- lib/client.py
+++ lib/client.py
@@ -1,5 +1,15 @@
 import socket
+import json
+import os
 
+def load_config():
+    """ Load config object from client configuration file """
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        '..', 'client.cfg')
+    with open(path) as f:
+        return json.loads(f.read())
+
+cfg = load_config()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('0.0.0.0', 23789))
+s.connect((cfg['server'], cfg['port']))
 s.send('hello')
